b912fa5c66f780f85f15b3bc114f0834982928e9
Rune Soerensen <runesoerensen@gmail.com>
2012-05-24 16:30:24 -0700

    Remove unused post properties

--- Core/Models/Post.cs
+++ Core/Models/Post.cs
@@ -1,46 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace Compilify.Models
+﻿namespace Compilify.Models
 {
-    public class Post
-    {
-        public Post()
-        {
-            Tags = new HashSet<string>();
-        }
-
-        public string Id { get; set; }
-        
-        public string Title { get; set; }
-
-        public string Slug { get; set; }
-
-        public int Version { get; set; }
-
-        public string Description { get; set; }
-
-        /// <summary>
-        /// The ID of the user who created the post, or null if the post was created by an anonymous user.</summary>
-        public Guid? AuthorId { get; set; }
-        
-        /// <summary>
-        /// If <c>true</c>, the post will not be displayed in search results. However, it will still be accessible via 
-        /// direct access.</summary>
-        public bool? IsPrivate { get; set; }
-
-        /// <summary>
-        /// The post content.</summary>
-        public string Content { get; set; }
-
-        /// <summary>
-        /// Supporting classes</summary>
-        public string Classes { get; set; }
-
-        public HashSet<string> Tags { get; protected set; }
-
-        /// <summary>
-        /// The UTC date and time that the post was first persisted to the data store.</summary>
-        public DateTime? Created { get; set; }
-    }
+	public class Post
+	{
+		public string Content { get; set; }
+		public string Classes { get; set; }
+	}
 }
