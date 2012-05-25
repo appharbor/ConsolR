847446daa93bb68ea3c68b4933ee6a4183f48925
Rune Soerensen <runesoerensen@gmail.com>
2012-05-25 03:26:19 -0700

    Move http handler extensions to hosting project

---
+++ Hosting/Web/HttpHandlerExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Web;
+using System.Web.Routing;
+
+namespace ConsolR.Hosting
+{
+	public static class HttpHandlerExtensions
+	{
+		public static void MapHttpHandler<THandler>(this RouteCollection routes, string url) where THandler : IHttpHandler, new()
+		{
+			routes.MapHttpHandler<THandler>(null, url, null, null);
+		}
+
+		public static void MapHttpHandler<THandler>(this RouteCollection routes,
+			string name, string url, object defaults, object constraints)
+			where THandler : IHttpHandler, new()
+		{
+			var route = new Route(url, new HttpHandlerRouteHandler<THandler>());
+			route.Defaults = new RouteValueDictionary(defaults);
+			route.Constraints = new RouteValueDictionary(constraints);
+			routes.Add(name, route);
+		}
+
+		private class HttpHandlerRouteHandler<THandler>
+			: IRouteHandler where THandler : IHttpHandler, new()
+		{
+			public IHttpHandler GetHttpHandler(RequestContext requestContext)
+			{
+				return new THandler();
+			}
+		}
+	}
+}
--- Web/ConsolRHandler.ashx.cs
+++ Web/ConsolRHandler.ashx.cs
@@ -12,6 +12,7 @@ using System.Web.Routing;
 using System.Web.Script.Serialization;
 using ConsolR.Core.Models;
 using ConsolR.Core.Services;
+using ConsolR.Hosting;
 using Newtonsoft.Json;
 using Roslyn.Compilers;
 using SignalR;
@@ -208,33 +209,6 @@ namespace ConsolR.Web
 				output.Append(hashBytes[i].ToString("x2"));
 			}
 			return output.ToString();
-		}
-	}
-
-	public static class HttpHandlerExtensions
-	{
-		public static void MapHttpHandler<THandler>(this RouteCollection routes, string url) where THandler : IHttpHandler, new()
-		{
-			routes.MapHttpHandler<THandler>(null, url, null, null);
-		}
-
-		public static void MapHttpHandler<THandler>(this RouteCollection routes,
-			string name, string url, object defaults, object constraints)
-			where THandler : IHttpHandler, new()
-		{
-			var route = new Route(url, new HttpHandlerRouteHandler<THandler>());
-			route.Defaults = new RouteValueDictionary(defaults);
-			route.Constraints = new RouteValueDictionary(constraints);
-			routes.Add(name, route);
-		}
-
-		private class HttpHandlerRouteHandler<THandler>
-			: IRouteHandler where THandler : IHttpHandler, new()
-		{
-			public IHttpHandler GetHttpHandler(RequestContext requestContext)
-			{
-				return new THandler();
-			}
 		}
 	}
 }
