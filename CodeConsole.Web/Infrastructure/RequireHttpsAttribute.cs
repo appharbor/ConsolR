0d3842438aae97db9637d099cb54a8fa15ad0ee9
Rune Soerensen <runesoerensen@gmail.com>
2012-05-24 17:43:53 -0700

    Format RequireHttpsAttribute

--- CodeConsole.Web/Infrastructure/RequireHttpsAttribute.cs
+++ CodeConsole.Web/Infrastructure/RequireHttpsAttribute.cs
@@ -3,38 +3,38 @@ using System.Web.Mvc;
 
 namespace ConsolR.Web.Infrastructure
 {
-    /// <summary>
-    /// Redirects HTTP requests to HTTPS.</summary>
-    /// <remarks>
-    /// Overriding the default behavior is necessary to avoid a redirect loop. See this article for details:
-    /// http://support.appharbor.com/kb/tips-and-tricks/ssl-and-certificates </remarks>
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = false)]
-    public sealed class RequireHttpsAttribute : System.Web.Mvc.RequireHttpsAttribute
-    {
-        public override void OnAuthorization(AuthorizationContext filterContext)
-        {
-            if (filterContext == null)
-            {
-                throw new ArgumentNullException("filterContext");
-            }
+	/// <summary>
+	/// Redirects HTTP requests to HTTPS.</summary>
+	/// <remarks>
+	/// Overriding the default behavior is necessary to avoid a redirect loop. See this article for details:
+	/// http://support.appharbor.com/kb/tips-and-tricks/ssl-and-certificates </remarks>
+	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = false)]
+	public sealed class RequireHttpsAttribute : System.Web.Mvc.RequireHttpsAttribute
+	{
+		public override void OnAuthorization(AuthorizationContext filterContext)
+		{
+			if (filterContext == null)
+			{
+				throw new ArgumentNullException("filterContext");
+			}
 
-            if (filterContext.HttpContext.Request.IsSecureConnection)
-            {
-                return;
-            }
+			if (filterContext.HttpContext.Request.IsSecureConnection)
+			{
+				return;
+			}
 
-            var header = filterContext.HttpContext.Request.Headers["X-Forwarded-Proto"];
-            if (string.Equals(header, "https", StringComparison.InvariantCultureIgnoreCase))
-            {
-                return;
-            }
+			var header = filterContext.HttpContext.Request.Headers["X-Forwarded-Proto"];
+			if (string.Equals(header, "https", StringComparison.InvariantCultureIgnoreCase))
+			{
+				return;
+			}
 
-            if (filterContext.HttpContext.Request.IsLocal)
-            {
-                return;
-            }
+			if (filterContext.HttpContext.Request.IsLocal)
+			{
+				return;
+			}
 
-            HandleNonHttpsRequest(filterContext);
-        }
-    }
+			HandleNonHttpsRequest(filterContext);
+		}
+	}
 }
