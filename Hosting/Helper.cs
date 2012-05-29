b69ddc4215ccc2e7973d0f005a7f5895902b9b22
Rune Soerensen <runesoerensen@gmail.com>
2012-05-28 18:02:54 -0700

    Add Helper class

---
+++ Hosting/Helper.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Web.Hosting;
+using SignalR;
+
+namespace ConsolR.Hosting
+{
+	public static class Helper
+	{
+		public static string ReadFile(string filename)
+		{
+			return File.ReadAllText(HostingEnvironment.MapPath(string.Format("~/{0}", filename)));
+		}
+
+		public static void Log(string input)
+		{
+			var endpoint = GlobalHost.ConnectionManager.GetConnectionContext<ExecuteEndPoint>().Connection;
+			var logMessage = new LogMessage(input);
+
+			endpoint.Broadcast(logMessage);
+		}
+
+		private class LogMessage
+		{
+			public LogMessage(string message)
+			{
+				data = message;
+			}
+
+			public string status { get { return "ok"; } }
+			public string data { get; private set; }
+		}
+	}
+}
